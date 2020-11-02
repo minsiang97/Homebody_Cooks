cc4c8533079e215a1d04cd2b000ce6aaeb4c26c7
Ong Min Siang <ongminsiang@gmail.com>
2020-11-02 23:26:08 +0800

    added payment gateway for subscription plans

--- homebody_cooks_web/__init__.py
+++ homebody_cooks_web/__init__.py
@@ -2,6 +2,7 @@ from app import app
 from flask import render_template
 from homebody_cooks_web.blueprints.users.views import users_blueprint
 from homebody_cooks_web.blueprints.sessions.views import sessions_blueprint
+from homebody_cooks_web.blueprints.transactions.views import transactions_blueprint
 from flask_assets import Environment, Bundle
 from .util.assets import bundles
 
@@ -10,6 +11,7 @@ assets.register(bundles)
 
 app.register_blueprint(users_blueprint, url_prefix="/users")
 app.register_blueprint(sessions_blueprint, url_prefix="/sessions")
+app.register_blueprint(transactions_blueprint, url_prefix="/subscription/<subscription_id>/transactions")
 
 @app.errorhandler(500)
 def internal_server_error(e):
