299580fbb4cc30d7417428f798421b2bc54a39ea
Ong Min Siang <ongminsiang@gmail.com>
2020-11-06 16:14:38 +0800

    modified

--- models/user.py
+++ models/user.py
@@ -73,8 +73,5 @@ class User(BaseModel, UserMixin):
 
 class MyAdminIndexView(AdminIndexView):
     def is_accessible(self):
-        if current_user.is_authenticated:
-            return current_user.is_admin
-        else :
-            return current_user.is_authenticated
+        return current_user.is_authenticated
         
