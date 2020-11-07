5392a00180223398459d5f2911169b052db6b9bf
Ong Min Siang <ongminsiang@gmail.com>
2020-11-07 15:31:34 +0800

    added recipes api

---
+++ homebody_cooks_api/blueprints/recipes/views.py
@@ -0,0 +1,24 @@
+from flask import Blueprint, jsonify
+from models.recipe import Recipe
+from flask_jwt_extended import jwt_required, get_jwt_identity
+
+recipes_api_blueprint = Blueprint('recipes_api',
+                             __name__,
+                             template_folder='templates')
+
+@recipes_api_blueprint.route('/', methods=['GET'])
+def index():
+    recipes = Recipe.select()
+    return jsonify([{"id" : r.id, "recipe_name" : r.recipe_name, "description" : r.description, "meal_type" : r.meal_type, "image_url" : u.image_url} for r in recipes])
+
+@recipes_api_blueprint.route('/vegetarian', methods=['GET'])
+@jwt_required
+def vegetarian() :
+    recipes = Recipe.select().where(Recipe.meal_type == "Vegetarian")
+    return jsonify([{"id" : r.id, "recipe_name" : r.recipe_name, "description" : r.description, "meal_type" : r.meal_type, "image_url" : u.image_url} for r in recipes])
+
+@recipes_api_blueprint.route('/mix', methods=['GET'])
+@jwt_required
+def mix() :
+    recipes = Recipe.select().where(Recipe.meal_type == "Mix")
+    return jsonify([{"id" : r.id, "recipe_name" : r.recipe_name, "description" : r.description, "meal_type" : r.meal_type, "image_url" : u.image_url} for r in recipes])
