72577b54e9a9e1910ae385ab271f1de82b43c7f5
Ong Min Siang <ongminsiang@gmail.com>
2020-11-07 15:46:47 +0800

    modified api

--- homebody_cooks_api/blueprints/recipes/views.py
+++ homebody_cooks_api/blueprints/recipes/views.py
@@ -1,6 +1,5 @@
 from flask import Blueprint, jsonify
 from models.recipe import Recipe
-from flask_jwt_extended import jwt_required, get_jwt_identity
 
 recipes_api_blueprint = Blueprint('recipes_api',
                              __name__,
@@ -9,14 +8,14 @@ recipes_api_blueprint = Blueprint('recipes_api',
 @recipes_api_blueprint.route('/', methods=['GET'])
 def index():
     recipes = Recipe.select()
-    return jsonify([{"id" : r.id, "recipe_name" : r.recipe_name, "description" : r.description, "meal_type" : r.meal_type, "image_url" : u.image_url} for r in recipes])
+    return jsonify([{"id" : r.id, "recipe_name" : r.recipe_name, "description" : r.description, "meal_type" : r.meal_type, "image_url" : r.image_url} for r in recipes])
 
 @recipes_api_blueprint.route('/vegetarian', methods=['GET'])
 def vegetarian() :
     recipes = Recipe.select().where(Recipe.meal_type == "Vegetarian")
-    return jsonify([{"id" : r.id, "recipe_name" : r.recipe_name, "description" : r.description, "meal_type" : r.meal_type, "image_url" : u.image_url} for r in recipes])
+    return jsonify([{"id" : r.id, "recipe_name" : r.recipe_name, "description" : r.description, "meal_type" : r.meal_type, "image_url" : r.image_url} for r in recipes])
 
 @recipes_api_blueprint.route('/mix', methods=['GET'])
 def mix() :
     recipes = Recipe.select().where(Recipe.meal_type == "Mix")
-    return jsonify([{"id" : r.id, "recipe_name" : r.recipe_name, "description" : r.description, "meal_type" : r.meal_type, "image_url" : u.image_url} for r in recipes])
+    return jsonify([{"id" : r.id, "recipe_name" : r.recipe_name, "description" : r.description, "meal_type" : r.meal_type, "image_url" : r.image_url} for r in recipes])
