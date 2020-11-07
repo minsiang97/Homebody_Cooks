4308764b0e3f9cbb2becfef34d52f8aa087e88a0
Ong Min Siang <ongminsiang@gmail.com>
2020-11-07 16:11:24 +0800

    added subscriptions api

--- models/subscription.py
+++ models/subscription.py
@@ -4,4 +4,4 @@ import peewee as pw
 class Subscription(BaseModel):
     name = pw.CharField(unique=True, null=False)
     amount_of_meals = pw.IntegerField(unique=False, null=False)
-    price = pw.DecimalField(null=False)
+    price = pw.IntegerField(null=False)
