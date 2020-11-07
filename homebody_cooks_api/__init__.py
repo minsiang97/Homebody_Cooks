e6ce581e47c02b2a0724571b53e2252e278df039
Ong Min Siang <ongminsiang@gmail.com>
2020-11-07 19:29:59 +0800

    added recipe ingredient api

--- homebody_cooks_api/__init__.py
+++ homebody_cooks_api/__init__.py
@@ -8,13 +8,16 @@ from homebody_cooks_api.blueprints.users.views import users_api_blueprint
 from homebody_cooks_api.blueprints.sessions.views import sessions_api_blueprint
 from homebody_cooks_api.blueprints.recipes.views import recipes_api_blueprint
 from homebody_cooks_api.blueprints.subscriptions.views import subscriptions_api_blueprint
+from homebody_cooks_api.blueprints.recipe_ingredients.views import recipe_ingredients_api_blueprint
 
 csrf.exempt(users_api_blueprint)
 csrf.exempt(sessions_api_blueprint)
 csrf.exempt(recipes_api_blueprint)
 csrf.exempt(subscriptions_api_blueprint)
+csrf.exempt(recipe_ingredients_api_blueprint)
 
 app.register_blueprint(users_api_blueprint, url_prefix='/api/v1/users')
 app.register_blueprint(sessions_api_blueprint, url_prefix='/api/v1/sessions')
 app.register_blueprint(recipes_api_blueprint, url_prefix='/api/v1/recipes')
 app.register_blueprint(subscriptions_api_blueprint, url_prefix='/api/v1/subscriptions')
+app.register_blueprint(recipe_ingredients_api_blueprint, url_prefix='/api/v1/recipe_ingredients')
