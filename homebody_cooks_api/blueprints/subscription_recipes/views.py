bf2d3a3988d0e55f22ffb7d6bdb1a7ca44a4d622
Ong Min Siang <ongminsiang@gmail.com>
2020-11-10 21:57:19 +0800

    modified api

--- homebody_cooks_api/blueprints/subscription_recipes/views.py
+++ homebody_cooks_api/blueprints/subscription_recipes/views.py
@@ -37,7 +37,7 @@ def today():
 def user_add_cart(recipe_id):
     user_id = get_jwt_identity()
     user = User.get_or_none(User.id == user_id)
-    ingredients = request.json.data.selectedIngredients
+    ingredients = request.json
     print(request.json.data.selectedIngredients)
     recipe = Recipe.get_or_none(Recipe.id == recipe_id)
     new_subscription_recipe = Subscription_Recipe(user = user.id, subscription = user.subscription.id, recipe = recipe.id)
