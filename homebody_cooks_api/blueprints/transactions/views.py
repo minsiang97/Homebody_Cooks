35c0c7919406659eaa4f72559cbfd00acb7d14ee
Ong Min Siang <ongminsiang@gmail.com>
2020-11-11 17:49:46 +0800

    added api

--- homebody_cooks_api/blueprints/transactions/views.py
+++ homebody_cooks_api/blueprints/transactions/views.py
@@ -1,10 +1,14 @@
 from flask import Blueprint, jsonify, request
 from models.user import User
 from models.order_checkout import OrderCheckout
+from models.subscription import Subscription
 from models.subscription_recipe import Subscription_Recipe
+from models.transaction import Transaction
 from flask_jwt_extended import jwt_required, get_jwt_identity
+from braintree.successful_result import SuccessfulResult
+from flask_mail import Message 
 from werkzeug.security import generate_password_hash, check_password_hash
-from app import app
+from app import app, send_message_first_payment
 from helpers import s3, upload_to_s3
 import datetime
 from helpers import gateway
@@ -22,3 +26,34 @@ def index():
     return jsonify({"token" : client_token})
 
 
+@transactions_api_blueprint.route('/<subscription_id>/payment', methods=['GET'])
+@jwt_required
+def payment(subscription_id):
+    user_id = get_jwt_identity()
+    user = User.get_or_none(User.id == user_id)
+    subscription = Subscription.get_or_none(Subscription.id == subscription_id)
+    nonce_from_the_client = request.json["payment_method_nonce"]
+    result = gateway.customer.create({
+        "first_name": user.name,
+        "email": user.email,
+        "payment_method_nonce": nonce_from_the_client
+    })
+
+    if result.is_success :
+        result_subscription = gateway.subscription.create({
+            "id" : user.id,
+            "payment_method_token": result.customer.payment_methods[0].token,
+            "plan_id": subscription.id
+        })
+
+    if type(result_subscription) == SuccessfulResult:
+        new_transaction = Transaction(amount = subscription.price, subscription = subscription.id , user = user.id)
+
+        if new_transaction.save():
+            user.is_valid = True
+            user.save()
+            send_message_first_payment.delay(email = user.email, name = user.name)
+            return jsonify({"message" : "Payment has been processed successfully"})
+
+    else :
+        return jsonify({"message" : "Payment failed, try again"})        
